b64da0220a5493e5b944025e656d9a21305325cc
aysan <aysanbidariiii@gmail.com>
2021-12-28 02:51:29 +0330

    App-f-added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import React ,{ useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './actions/TodoActions';
 import './App.css';
 
 
 
 function App() {
+  
+  const [todo , setTodo] = useState();
+  
+  const dispatch = useDispatch();
+
+  const Todo = useSelector((state) => state.Todo);
+
+  const {todos} = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+    
+  };
+
+  const removeHandler = (t) => {
+    dispatch(RemoveTodoAction(t));
+  };
+ 
+
 
   return (
     <div className="App">
       <header className="App-header">
        <h2>Todo List (react-redux)</h2> 
-       <form>
-         <input className='todo-input' placeholder='Enter Todo'/>
-         <button className='todo-input-btn' type='submit'>Click me</button>
-       </form>
+       <form onSubmit={handleSubmit}>
+         <input className='todo-input' placeholder='Enter Todo' onChange={(e)=> setTodo(e.target.value)}/>
+         <button className='todo-input-btn' type="submit">Click me</button>
+       </form >
        <ul className='all-todos'>
-         <li className='single-todo'>
-           <span className='todo-text'>First Todo</span>
-           <button className='li-btn'>Delete</button>
-         </li>
+         {
+           todos && todos.map((t) => (
+            <li key={t.id} className='single-todo'>
+            <span className='todo-text'>{t.todo}</span>
+            <button className='li-btn' onClick={()=>removeHandler(t)}>Delete</button>
+          </li>
+           ))
+         }
+        
        </ul>
 
         
